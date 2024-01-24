ac86c0fe87e1e454fd014c0ea88ad58f46632261
J-Alexander99 <joelalexanderdonaldson@gmail.com>
2024-01-24 22:40:58 +0000

    Adding the files done so far

---
+++ myScript.js
@@ -0,0 +1,14 @@
+let userInput;
+let daysLeft;
+let updateCount;
+userInput = document.getElementById("wishesSaved");
+daysLeft = document.getElementById("daysLeft");
+updateCount = document.getElementById("updateCount");
+
+
+
+
+
+
+let x = "200";
+document.getElementById("wishes").innerHTML = x;
